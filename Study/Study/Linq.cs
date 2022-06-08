2546756cde2f6b2ab457f2e27eaf9dc3a3132e13
ntz448 <77314033+natizen448@users.noreply.github.com>
2022-06-08 09:46:17 +0900

    즉시 실행

--- Study/Study/Linq.cs
+++ Study/Study/Linq.cs
@@ -17,7 +17,7 @@ namespace Study
             };
 
             //IEnumerable<String> query = names.Where(s => s.Length <= 5).Select(s => s.ToLower()); // Where 메서드를 통해 추출한 문자를 소문자로 변경
-                                                                                                    // 이처럼 메서드를 연결하는 것을 메서드 체인이라고 한다.
+            // 이처럼 메서드를 연결하는 것을 메서드 체인이라고 한다.
 
             // 실제 코드에서는 일반적으로 var 키워드를 사용해 다음과 같이 작성한다.
             //var query = names.Where(s => s.Length <= 5).Select(s => s.ToLower());
@@ -50,7 +50,27 @@ namespace Study
             //// 이를 보고 알 수 있는 사실은 이 query 변수에는 검색된결과가 대입되는 것이 아니라, Where 메서드가 호출돼도 검색은 해당 시점에서 실행되지 않고
             //// 실제 값이 필요할 때 쿼리가 실행된다는 것을 나타낸다.
             //// 데이터가 필요할 때마다 쿼리가 실행되는 것이 Linq의 큰 특징이다.
-            
+
+            //// 즉시 실행
+            //var query = names.Where(s => s.Length <= 5).ToArray();
+            //foreach (var item in query)
+            //    Console.WriteLine(item);
+
+            //Console.WriteLine("---------");
+
+            //names[0] = "Busan";
+            //foreach (var item in query)
+            //    Console.WriteLine(item);
+
+            //// 결과
+            //// Seoul
+            //// Paris
+            //// ------
+            //// Seoul
+            //// Paris
+            //// 이렇게 하면 ToArray가 메서드가 호출됐을 때 쿼리가 실행되며 그 결과가 배열에 저장됩니다.
+            //// 이와 같은 메서드들은 ToList, Count등이 있습니다.
+
         }
     }
 }
