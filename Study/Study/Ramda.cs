2bf55d9c94be916f029852f57b3460ca4db7a8c0
ntz448 <77314033+natizen448@users.noreply.github.com>
2022-06-08 09:26:44 +0900

    메서드 체인 실제 사용 예시

--- Study/Study/Ramda.cs
+++ Study/Study/Ramda.cs
@@ -10,17 +10,22 @@ namespace Study
     class Ramda
     {
  
-        private void Linq()
+        private void MethodChaining()
         {
             var names = new List<string>
             {
                 "Seoul","New Delhi",
             };
 
-            IEnumerable<String> query = names.Where(s => s.Length <= 5).Select(s => s.ToLower()); // Where 메서드를 통해 추출한 문자를 소문자로 변경
+            // IEnumerable<String> query = names.Where(s => s.Length <= 5).Select(s => s.ToLower()); // Where 메서드를 통해 추출한 문자를 소문자로 변경
                                                                                                   // 이처럼 메서드를 연결하는 것을 메서드 체인이라고 한다.
+
+            // 실제 코드에서는 일반적으로 var 키워드를 사용해 다음과 같이 작성한다.
+            var query = names.Where(s => s.Length <= 5).Select(s => s.ToLower());
 
 
         }
+
+  
     }
 }
