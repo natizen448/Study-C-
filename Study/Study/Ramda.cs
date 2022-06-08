032427bd85d71983642d21e72dda13d082de201a
ntz448 <77314033+natizen448@users.noreply.github.com>
2022-06-08 09:20:44 +0900

    메서드 체인
    메서드를 서로 연결하는것.

---
+++ Study/Study/Ramda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{   
+    
+    class Ramda
+    {
+ 
+        private void Linq()
+        {
+            var names = new List<string>
+            {
+                "Seoul","New Delhi",
+            };
+
+            IEnumerable<String> query = names.Where(s => s.Length <= 5).Select(s => s.ToLower()); // Where 메서드를 통해 추출한 문자를 소문자로 변경
+                                                                                                  // 이처럼 메서드를 연결하는 것을 메서드 체인이라고 한다.
+
+
+        }
+    }
+}
