7986069aab52450d828d7270baec42b4b16ca151
ntz448 <77314033+natizen448@users.noreply.github.com>
2022-06-08 09:31:44 +0900

    파일 정리

--- Study/Study/Linq.cs
+++ Study/Study/Linq.cs
@@ -5,11 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Study
-{   
-    
-    class Ramda
+{
+    class Linq
     {
- 
+
         private void MethodChaining()
         {
             var names = new List<string>
@@ -18,14 +17,17 @@ namespace Study
             };
 
             // IEnumerable<String> query = names.Where(s => s.Length <= 5).Select(s => s.ToLower()); // Where 메서드를 통해 추출한 문자를 소문자로 변경
-                                                                                                  // 이처럼 메서드를 연결하는 것을 메서드 체인이라고 한다.
+            // 이처럼 메서드를 연결하는 것을 메서드 체인이라고 한다.
 
             // 실제 코드에서는 일반적으로 var 키워드를 사용해 다음과 같이 작성한다.
-            var query = names.Where(s => s.Length <= 5).Select(s => s.ToLower());
+            //var query = names.Where(s => s.Length <= 5).Select(s => s.ToLower());
 
-
+            // Select만 사용한 예시
+            var query = names.Select(s => s.Length);
+            foreach (var n in query)
+            {
+                Console.WriteLine("{0} ", n);
+            }
         }
-
-  
     }
 }
